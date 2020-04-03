045a56c4eb4c37a1dfe475cea9966a8e2c8a8521
João Henrique <costa.joaohenrique@gmail.com>
2020-04-03 02:29:51 -0300

    Adicionando body-parser e criando rota para listar todas as lojas

--- src/config/server.js
+++ src/config/server.js
@@ -1,12 +1,15 @@
 var express = require("express")
 var consign = require("consign")
+var bodyParser = require("body-parser")
 
 module.exports = () => {
     var app = express()
 
+    app.use(bodyParser.json())
+
     consign()
         .include("src/controllers")
         .into(app)
-    
+
     return app
 }
--- src/controllers/diapers.js
+++ src/controllers/diapers.js
@@ -1,20 +1,30 @@
 const MongoClient = require('mongodb').MongoClient
 const uri = "mongodb://find_diapers:find_diapers@localhost:27017/?authSource=admin"
 
-
 module.exports = (app) => {
-    app.get('/diapers', function (req, resp) {
+    app.get('/diapers', (req, resp) => {
         console.log("Primeira requisição recebiida")
         MongoClient.connect(uri, (err, client) => {
+            if (err) throw err;
             var db = client.db("find-diapers")
-            var diaper = db.collection("diapers").find().toArray((error, result) => {
-                console.log(result)
-
-                resp.send(result)
-
+            db.collection("diapers").find().toArray((error, result) => {
+                if (error) console.error("Erro ao buscar os produtos no banco")
+                resp.json(result)
             })
-
-
         })
     })
+
+    app.get('/stores', (req, resp) => {
+        console.log("Primeira requisição recebiida")
+        MongoClient.connect(uri, (err, client) => {
+            if (err) throw err;
+            var db = client.db("find-diapers")
+            var query = 
+            db.collection("diapers").distinct("store", (error, result) => {
+                resp.json(result)
+            } )
+        })
+    })
+
+    
 }
